806644d8ea171b3c5dfd4703f4131f9f95f47079
Quinn Yockey <qyockey@tutanota.de>
2024-06-29 21:44:19 -0700

    use iterators and closures over loops

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
+        eprintln!("Usage: $ cargo run -- query file_path");
         process::exit(1);
     });
 
